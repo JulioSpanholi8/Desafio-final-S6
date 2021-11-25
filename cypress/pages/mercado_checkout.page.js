82bea2912575187663548e27d56eb62c7ca45553
Julio Cesar Spanholi <jcspanholi30@hotmail.com>
2021-11-25 16:01:33 -0300

    Atualizando checkout e report

--- cypress/pages/mercado_checkout.page.js
+++ cypress/pages/mercado_checkout.page.js
@@ -21,18 +21,19 @@ export default class MOCheckout extends Base {
     }
     static realizarCheckout(){
         cy.readFile('cypress/fixtures/cadastro_endereco/endereco.json').then(endereco =>{ 
-            cy.get('.ch-modal-content').should('not.be.visible')
+            cy.get(CK.CHECK_MODAL).should('not.be.visible')
                 cy.readFile('cypress/fixtures/cadastro_usuario/userValido.json').then(usuario => {
-                cy.get("[data-tray-tst=security_question]").then($securityQuestion => {
-                    if ($securityQuestion.find("p:contains('Qual seu endereço de entrega?')").length > 0){
+                cy.get(CK.CHECK_PGT).then($security => {
+                    if ($security.find("p:contains('Qual seu endereço de entrega?')").length > 0){
                         selecionar = endereco.valido.rua.split(' ')[1]
-                    } else if ($securityQuestion.find("p:contains('Quais os primeiros digitos do seu CPF?')").length > 0) {
+                    } else if ($security.find("p:contains('Quais os primeiros digitos do seu CPF?')").length > 0) {
                         selecionar = usuario.valido.cpf.substring(0,6)
-                    } else if ($securityQuestion.find("p:contains('Quais os últimos digitos do seu CPF?')").length > 0){
+                    } else if ($security.find("p:contains('Quais os últimos digitos do seu CPF?')").length > 0){
                         selecionar = usuario.valido.cpf.slice(-6) 
-                    } else if ($securityQuestion.find("p:contains('Qual o seu sobrenome?')").length > 0){
+                    } else if ($security.find("p:contains('Qual o seu sobrenome?')").length > 0){
                         selecionar = usuario.valido.nome.split(' ')[1]   
                     }
+                    cy.wait(3000)
                     super.verifyElement(CK.BTN_SELECIONAR)                    
                     super.getElement(CK.BTN_SELECIONAR).contains(selecionar).click()
                 })
